92d649cdada9f15cb9785e903d7cd82a05038111
Olena Hryhorzhevska <elenahry8@gmail.com>
2026-01-28 21:53:23 -0500

    home page

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,7 +1,65 @@
-export default function Home() {
+import { Link } from "react-router-dom";
+import css from "./Home.module.css";
+
+export default function HomePage() {
   return (
-    <div>
-      <h1>Welcome to the Home Page</h1>
-    </div>
-  )
+    <main className="container">
+      <section className={css.heroCard}>
+        <header className={css.header}>
+          <Link className={css.logo} to="/">
+            <img
+              src="/images/logowhite.png"
+              alt="Petlove"
+              className={css.logoImage}
+            />
+          </Link>
+
+          <nav className={css.nav}>
+            <a className={css.navLink} href="#news">
+              News
+            </a>
+            <a className={css.navLink} href="#find">
+              Find pet
+            </a>
+            <a className={css.navLink} href="#friends">
+              Our friends
+            </a>
+          </nav>
+
+          <div className={css.auth}>
+            <Link className={css.loginBtn} to="/login">
+              Log in
+            </Link>
+            <Link className={css.registerBtn} to="/register">
+              Registration
+            </Link>
+          </div>
+        </header>
+
+        <div className={css.heroContent}>
+          <h1 className={css.title}>
+            Take good <span className={css.titleLight}>care</span> of your small
+            pets
+          </h1>
+
+          <p className={css.subtitle}>
+            Choosing a pet for your home is a choice that is meant to enrich
+            your life with immeasurable joy and tenderness.
+          </p>
+        </div>
+      </section>
+
+      <section className={css.imageCard}>
+        <img
+          className={css.image}
+          src="/images/hero-photo-1x.jpg"
+          srcSet="
+    /images/hero-photo-1x.jpg 1x,
+    /images/hero-photo-2x.jpg 2x
+  "
+          alt="Woman hugging a dog"
+        />
+      </section>
+    </main>
+  );
 }

e72157e0ee49f793ac368b14e6ff35d071f596ea
Olena Hryhorzhevska <elenahry8@gmail.com>
2026-02-13 21:02:01 -0500

    burger menu added for mobiles and tablets, styles applied

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,47 +1,109 @@
 import { Link, NavLink } from "react-router-dom";
 import styles from "./Header.module.css";
+import { useState } from "react";
 
 type Props = {
   variant?: "dark" | "light";
 };
 
 export default function Header({ variant = "dark" }: Props) {
+  const [modalOpen, setModalOpen] = useState(false);
 
   const getNavClass = ({ isActive }: { isActive: boolean }) =>
     `${styles.navLink} ${isActive ? styles.navBorder : ""}`;
 
+  const openMenu = () => {
+    setModalOpen(true);
+  };
+
+  const closeMenu = () => {
+    setModalOpen(false);
+  };
+
   return (
-    <header className={`${styles.header} ${styles[variant]}`}>
-      <Link className={styles.logo} to="/">
-        <img
-          src={
-            variant === "dark" ? "/images/logowhite.png" : "/images/logo.png"
-          }
-          alt="Petlove"
-          className={styles.logoImage}
-        />
-      </Link>
-
-      <nav className={styles.nav}>
-        <NavLink className={getNavClass} to="/news">
-          News
-        </NavLink>
-        <NavLink className={getNavClass} to="/find">
-          Find pet
-        </NavLink>
-        <NavLink className={getNavClass} to="/friends">
-          Our friends
-        </NavLink>
-      </nav>
-
-      <div className={styles.auth}>
-        <Link className={styles.loginBtn} to="/login">
-          Log in
+    <>
+      <header className={`${styles.header} ${styles[variant]}`}>
+        <Link className={styles.logo} to="/">
+          <img
+            src={
+              variant === "dark" ? "/images/logowhite.png" : "/images/logo.png"
+            }
+            alt="Petlove"
+            className={styles.logoImage}
+          />
         </Link>
-        <Link className={styles.registerBtn} to="/register">
-          Registration
-        </Link>
-      </div>
-    </header>
+        <nav className={styles.nav}>
+          <NavLink className={getNavClass} to="/news">
+            News
+          </NavLink>
+          <NavLink className={getNavClass} to="/find">
+            Find pet
+          </NavLink>
+          <NavLink className={getNavClass} to="/friends">
+            Our friends
+          </NavLink>
+        </nav>
+        <div className={styles.auth}>
+          <Link className={styles.loginBtn} to="/login">
+            Log in
+          </Link>
+          <Link className={styles.registerBtn} to="/register">
+            Registration
+          </Link>
+        </div>
+        <button
+          type="button"
+          className={styles.burgerBtn}
+          onClick={openMenu}
+          aria-label="Open menu"
+        >
+          ☰
+        </button>
+      </header>
+      {modalOpen && (
+        <div className={styles.menuOverlay} role="dialog" aria-modal="true">
+          <div className={styles.menuPanel}>
+            <div className={styles.menuTop}>
+              <button
+                type="button"
+                className={styles.closeBtn}
+                onClick={closeMenu}
+                aria-label="Close menu"
+              >
+                ✕
+              </button>
+            </div>
+
+            <nav className={styles.menuNav}>
+              <NavLink className={getNavClass} to="/news" onClick={closeMenu}>
+                News
+              </NavLink>
+              <NavLink className={getNavClass} to="/find" onClick={closeMenu}>
+                Find pet
+              </NavLink>
+              <NavLink
+                className={getNavClass}
+                to="/friends"
+                onClick={closeMenu}
+              >
+                Our friends
+              </NavLink>
+            </nav>
+            <div className={styles.menuAuth}>
+              <Link className={styles.menuLoginBtn} to="/login" onClick={closeMenu}>
+                Log in
+              </Link>
+              <Link
+                className={styles.menuRegisterBtn}
+                to="/register"
+                onClick={closeMenu}
+              >
+                Registration
+              </Link>
+            </div>
+          </div>
+        </div>
+      )}
+    </>
   );
 }
--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -5,7 +5,7 @@ export default function HomePage() {
   return (
     <main className="container">
       <section className={css.heroCard}>
-        <Header variant="dark"/>
+        <Header variant="dark" />
         <div className={css.heroContent}>
           <h1 className={css.title}>
             Take good <span className={css.titleLight}>care</span> of your small
@@ -22,10 +22,10 @@ export default function HomePage() {
       <section className={css.imageCard}>
         <img
           className={css.image}
-          src="/images/hero-photo-1x.jpg"
+          src="/images/tab-hero-photo-1x.jpg"
           srcSet="
-    /images/hero-photo-1x.jpg 1x,
-    /images/hero-photo-2x.jpg 2x
+    /images/tab-hero-photo-1x.jpg 1x,
+    /images/tab-hero-photo-2x.jpg 2x
   "
           alt="Woman hugging a dog"
         />

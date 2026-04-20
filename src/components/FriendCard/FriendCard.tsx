ea980642768a06387d5f100f93370bab9d943270
Olena Hryhorzhevska <elenahry8@gmail.com>
2026-04-20 16:16:23 -0400

    friendcard component updated

--- src/components/FriendCard/FriendCard.tsx
+++ src/components/FriendCard/FriendCard.tsx
@@ -6,14 +6,36 @@ export default function FriendCard({ friend }: { friend: FriendsItem }) {
   return (
     <>
       <li className={styles.friendCard}>
-        <img className={styles.friendImg} src={friend.imageUrl} alt="friend image" width={90} height={90}/>
+        <img
+          className={styles.friendImg}
+          src={friend.imageUrl}
+          alt="friend image"
+          width={90}
+          height={90}
+        />
         <div>
-          <h3>{friend.title}</h3>
-          <p>Email: {friend.email}</p>
-          <p>Address: {friend.address ? friend.address : "website only"}</p>
-          <p>Phone: {friend.phone ? friend.phone : "email only"}</p>
+          <h3 className={styles.friendTitle}>{friend.title}</h3>
+          <div className={styles.friendDetails}>
+            <p className={styles.friendInfoGrey}>
+              Email: <span className={styles.friendInfo}>{friend.email}</span>
+            </p>
+            <p className={styles.friendInfoGrey}>
+              Address:{" "}
+              <span className={styles.friendInfo}>
+                {friend.address ? friend.address : "website only"}
+              </span>
+            </p>
+            <p className={styles.friendInfoGrey}>
+              Phone:{" "}
+              <span className={styles.friendInfo}>
+                {friend.phone ? friend.phone : "email only"}
+              </span>
+            </p>
+          </div>
         </div>
-        <p className={styles.workingTime}>{openDay ? `${openDay.from} - ${openDay.to}` : "Day and night"}</p>
+        <p className={styles.workingTime}>
+          {openDay ? `${openDay.from} - ${openDay.to}` : "Day and night"}
+        </p>
       </li>
     </>
   );
